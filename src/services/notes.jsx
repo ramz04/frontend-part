55bc5b9c1898e3cce00753e877ec2100cf93e80b
ramz04 <wunpini95@gmail.com>
2023-11-22 09:53:28 +0000

    Update notes.jsx

--- src/services/notes.jsx
+++ src/services/notes.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-const baseUrl = "http://localhost:3000/api/notes"
+const baseUrl = "https://express-app-test-mtpl.onrender.com/api/notes"
 
 const getAll = () => {
   const request = axios.get(baseUrl)
